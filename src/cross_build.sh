650961260ef02a9eccbc32d335d8fbdd78e29291
Jemy <jemygraw@gmail.com>
2016-10-12 18:19:44 +0800

    publish new build scripts

---
+++ src/cross_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+DIR=$(cd ../; pwd)
+export GOPATH=$GOPATH:$DIR
+gox -os="windows" -arch="386"   -output="../bin/qlive_windows_386"
+gox -os="windows" -arch="amd64" -output="../bin/qlive_windows_amd64"
+gox -os="darwin"  -arch="386"   -output="../bin/qlive_darwin_386"
+gox -os="darwin"  -arch="amd64" -output="../bin/qlive_darwin_amd64"
+gox -os="linux"   -arch="386"   -output="../bin/qlive_linux_386"
+gox -os="linux"   -arch="amd64" -output="../bin/qlive_linux_amd64"
+gox -os="linux"   -arch="arm"   -output="../bin/qlive_linux_arm"
